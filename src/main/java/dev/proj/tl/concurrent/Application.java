9d062aa8777a6900057013948ff5ff8ae739233e
lubing <1113706636@qq.com>
2020-07-14 23:26:52 +0800

    1 commit

--- src/main/java/dev/proj/tl/concurrent/Application.java
+++ src/main/java/dev/proj/tl/concurrent/Application.java
@@ -2,6 +2,6 @@ package dev.proj.tl.concurrent;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("ok!");
+        System.out.println("home ok!");
     }
 }
